f36523eed2949fae4df0ba677a5ba2d140ed8e92
Marie Treschow <treschow.marie@gmail.com>
2016-10-19 12:31:05 +0200

    second commit

---
+++ index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var pug = require('pug');
+var app = express();
+
+app.use(express.static(__dirname + '/'));
+
+app.set('view engine', './pug');
+app.set('views', './views');
+
+app.get('/', function(request, response) {
+  response.render('/application.pug');
+});
+
+app.get('/', function(request, response) {
+
+  response.sendFile(__dirname + '/index.html');
+});
+
+app.get('/', function(request, response) {
+
+  response.sendFile(__dirname + '/index2.html');
+});
+
+app.get('/', function(request, response) {
+
+  response.sendFile(__dirname + '/index3.html');
+});
+
+app.get('/', function(request, response) {
+
+  response.sendFile(__dirname + '/index4.html');
+});
+
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!');
+});
